ae86ec3bb3c8aa10a20c79fb6efaf43694ec34d7
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-11 00:44:28 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,8 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 }).catch(e => console.error(e));
 
-
-let currentPopup: any = undefined;
 let currentZone: string;
+let currentPopup: any;
 
 const config = [
     {
@@ -80,34 +79,6 @@ function openPopup(zoneName: string, popupName: string) {
 }
 function closePopup(){
     if (typeof currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
-
-
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-    
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-    })
-
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-    
-}).catch(e => console.error(e));
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
