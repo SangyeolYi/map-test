752b5e46c4abd174d4b9b438d2fe166fba1e0b04
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-10 21:37:28 +0900

    a

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const menu = WA.ui.registerMenuCommand('menu test',
-    {
-        callback: () => {
-            WA.chat.sendChatMessage('test');
-        }
-    })
-
-// Some time later, if you want to remove the menu:
-menu.remove();
+WA.ui.registerMenuCommand('menu test', {
+    callback: () => {
+        WA.chat.sendChatMessage('test');
+    }
+})
