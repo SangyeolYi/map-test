13a8ce5839b934990ded15923afb91883996bb6f
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-11 00:15:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ WA.ui.registerMenuCommand('menu test', {
         WA.chat.sendChatMessage('test');
     }
 })
+
+
+WA.ui.registerMenuCommand('menu test', {
+    callback: () => {
+        WA.chat.sendChatMessage('test');
+    }
+})
+
+const myLayerSubscriber = WA.room.onEnterLayer("j-sleore").subscribe(() => {
+    WA.chat.sendChatMessage("Hello!", "Mr Robot");
+});
+
+WA.room.onLeaveLayer("j-sleore").subscribe(() => {
+    WA.chat.sendChatMessage("Goodbye!", "Mr Robot");
+    myLayerSubscriber.unsubscribe();
+});
