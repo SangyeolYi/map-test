7a12dd7e7cf8500d274e6903b7b091b52ed1061c
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-11 00:21:26 +0900

    q

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ WA.room.onLeaveLayer("j-sleore").subscribe(() => {
     WA.chat.sendChatMessage("Goodbye!", "Mr Robot");
     myLayerSubscriber.unsubscribe();
 });
+
+
+WA.onEnterZone('needHelp', () => {
+    currentZone = 'needHelp'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.onEnterZone('followUs', () => {
+    currentZone = 'followUs'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.onLeaveZone('needHelp', closePopup);
+WA.onLeaveZone('followUs', closePopup);
+
+
+function openPopup(zoneName: string, popupName: string) {
+    const zone = config.find((item) => {
+        return item.zone == zoneName
+    });
+    if (typeof zone !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
+    }
+}
+function closePopup(){
+    if (typeof currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
