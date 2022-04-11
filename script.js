5365c851a549388619cf33c01c4b07d31582c955
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-11 16:58:12 +0900

    1

--- script.js
+++ script.js
@@ -890,7 +890,7 @@ WA.onInit().then(() => {
         // }
 
         const triggerMessage = WA.ui.displayActionMessage({
-            message: "SSSSSSpaceキーかタッチで参加できます",
+            message: "Spaceキーかタッチで参加できます",
             callback: () => {
                 WA.nav.goToPage('https://jitsi.katariba.online/globalroomkmelonpann#userInfo.displayName="'+ userName + '"');
                 // WA.chat.sendChatMessage("confirmed", "trigger message logic")
