c5b2ea980b4114238ce786a124b36b3e7f0d7bf8
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2022-04-12 12:37:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -887,7 +887,7 @@ WA.onInit().then(() => {
     let mtgRoomNames = ['melonpann','rollcake','creampann','shiopann','cinnamonroll','currypann','chocolatecookie']
 
     // Androidの6~8までは、JITSIに接続する際に、WAを終了してJITSIに直接REDIRECTさせる
-    if(ua.indexOf("android 6.") !== -1 || ua.indexOf("android 7.") !== -1 || ua.indexOf("android 8.") !== -1) {
+    if(ua.indexOf("android 3.") !== -1 ||ua.indexOf("android 4.") !== -1 ||ua.indexOf("android 5.") !== -1 ||ua.indexOf("android 6.") !== -1 || ua.indexOf("android 7.") !== -1 || ua.indexOf("android 8.") !== -1) {
     
 
         for (let s = 0; s < mtgRoomNames.length; s++){
