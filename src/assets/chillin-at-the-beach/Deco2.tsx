d7715beaf14314758eff8ff947b4aab151b8819e
Kenichi Wakabayashi <kwaka1208@crssrds.jp>
2022-07-25 11:42:45 +0900

    船のタイルセットを追加

---
+++ src/assets/chillin-at-the-beach/Deco2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Deco2" tilewidth="32" tileheight="32" tilecount="760" columns="40">
+ <image source="Deco2.png" width="1280" height="608"/>
+</tileset>
---
+++ src/assets/chillin-at-the-beach/Deco3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Deco3" tilewidth="32" tileheight="32" tilecount="2304" columns="32">
+ <image source="Deco3.png" width="1024" height="2304"/>
+</tileset>
