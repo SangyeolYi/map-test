925e43658b8a42a05d0697664040bccebe2f7f3e
SangyeolYi <41245913+SangyeolYi@users.noreply.github.com>
2023-02-18 19:44:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,179 +1,102 @@
-// import zoomMtg from "./zoomSdk.js"
-// import  {zoomMtg}  from "./zoomSDK.js";
-
-
-// signature=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-// eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTYyNTgsImV4cCI6MTY2ODk2MzQ1OCwibW4iOjUzNTY0MTUyNjIsInJvbGUiOjB9.
-// NUz8RT1iq5ded4Z8OEQ_xiTU59Zn0jSrKjqhs5liiq0
-// signature=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-// eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTYyNzcsImV4cCI6MTY2ODk2MzQ3NywibW4iOjUzNTY0MTUyNjIsInJvbGUiOjB9.
-// c6pR31Gik5RdeFjk-A7FVPeXz6LzfVygK-Yf-Ha6Jbs
-
-// https://zoom-sdk-web.vercel.app:/meeting.html?name=YXNkYXNk&mn=5356415262&email=&pwd=&role=0&lang=jp-JP&
-// signature=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTY4NDcsImV4cCI6MTY2ODk2NDA0NywibW4iOjUzNTY0MTUyNjIsInJvbGUiOjB9.
-// bM2XlxJhjjn7gPCAlz1iaHYViJzY9eQ-gNwv5KwvzQo&china=1&sdkKey=Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo
-// https://zoom-sdk-web.vercel.app:/meeting.html?name=YXNkYXNk&mn=5356415262&email=&pwd=&role=0&lang=jp-JP&
-// signature=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTY4NjksImV4cCI6MTY2ODk2NDA2OSwibW4iOjUzNTY0MTUyNjIsInJvbGUiOjB9.
-// 6FsEV0cUmtTLgHtl3GIgonl2W2Lj2UEtVLsbShIqYiI&china=1&sdkKey=Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo
-
-// https://zoom-sdk-web.vercel.app:/meeting.html?name=YXNkYXNk&mn=3715273694&email=&pwd=&role=0&lang=jp-JP&
-// signature=
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTY5MzcsImV4cCI6MTY2ODk2NDEzNywibW4iOjM3MTUyNzM2OTQsInJvbGUiOjB9.
-// dU0VyJXBH8P17c7ylHpq4vg8Yd9OMmDvBJMY53G4LHI&china=1&sdkKey=Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTgxNzksImV4cCI6MTY2ODk2NTM3OSwibW4iOiI1MzU2NDE1MjYyIiwicm9sZSI6MX0.
-// anxa7qZ7HsjGcvz1d0UihoTN7U6bA6ZVz8gAwYPklI4
-
-// https://zoom-sdk-web.vercel.app:/meeting.html?name=YXNkYXNk&mn=5356415262&email=&pwd=&role=0&lang=jp-JP&signature=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NTY5MzcsImV4cCI6MTY2ODk2NDEzNywibW4iOjM3MTUyNzM2OTQsInJvbGUiOjB9.dU0VyJXBH8P17c7ylHpq4vg8Yd9OMmDvBJMY53G4LHI&china=1&sdkKey=Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo
-
-
-// generateSDKSignature:
-
-// function(e){
-//   x(e);
-//   var t,
-//   n=e.sdkKey,
-//   i=e.sdkSecret,
-//   a=e.meetingNumber,
-//   s=e.role,
-//   c=Math.round((new Date).getTime()/1e3)-30,
-
-  // u={
-  //   sdkKey:n,
-  //   iat:c,
-  //   exp:c+7200,
-  //   mn:"5356415262",
-  //   role:1
-  // },
-//     l=JSON.stringify({alg:"HS256",typ:"JWT"}),
-//     d=JSON.stringify(u);
-//     return""
-//   !==(t=r.fs.jws.JWS.sign("HS256",l,d,i))?(0,o.aY)(e,"generateSDKSignature",!0,t):(0,o.Ue)(e,"generateSDKSignature",!0),t
-// }
-
-// console.log(zoomMtg)
-// import jws from 'jws'
-
-// const c=Math.round((new Date).getTime()/1e3)-30
-// const u={
-//   sdkKey:"Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo",
-//   iat:c,
-//   exp:c+7200,
-//   mn:"5356415262",
-//   role:0
-// }
-// const sdkSecret = "5hUmfqKVLNPBiil39HS8VusLlYQK90C7xahs"
-// const l=JSON.stringify({alg:"HS256",typ:"JWT"})
-// const d=JSON.stringify(u);
-
-// const signature = jws.sign({
-//   header: {alg:"HS256",typ:"JWT"},
-//   payload: u,
-//   secret: sdkSecret,
-// });
-
-// console.log(signature)
-// // console.log(Math.round((new Date).getTime()/1e3)-30)
-
-// const zoomMeetingIDs = {
-//   "36":"5356415262",
-//   "37":"3715273694",
-// }
-// console.log(zoomMeetingIDs[40])
-
 WA.onInit().then(() => {
-  WA.nav.goToPage('https://play.vs.katariba.online/_/global/play.vs.katariba.online/maps/room-k/map.json');
-  // console.log('Scripting API ready');
-  // console.log('Player tags: ',WA.player.tags)
-  // let ua = window.navigator.userAgent.toLowerCase();
-  // let userName = WA.player.name
-  // let triggerMessage
-  
-  // let mtgRoomNames = ['melonpann'
-  //                     ,'rollcake'
-  //                     ,'creampann'
-  //                     ,'shiopann'
-  //                     ,'cinnamonroll'
-  //                     ,'currypann'
-  //                     ,'chocolatecookie'
-  //                     ,'shinryokuRight'
-  //                     ,'shinryokuLeft'
-  //                     ,'sanbashi'
-  //                     ,'roomk-shinden'
-  //                     ,'Kilimanjaro'
-  //                     ,'MountFuji'
-  //                     ,'Everest'
-  //                     ,'Rocky'
-  //                     ,'StudyArea100'
-  //                     ,'StudyArea101'
-  //                     ,'StudyArea102'
-  //                     ,'StudyArea103'
-  //                     ,'StudyArea104'
-  //                     ,'StudyArea105'
-  //                     ,'StudyArea106'
-  //                     ,'StudyArea107'
-  //                     ,'StudyArea108'
-  //                     ,'StudyArea200'
-  //                     ,'StudyArea201'
-  //                     ,'StudyArea202'
-  //                     ,'StudyArea203'
-  //                     ,'StudyArea204'
-  //                     ,'StudyArea205'
-  //                     ,'StudyArea206'
-  //                     ,'StudyArea207'
-  //                     ,'StudyArea208'
-  //                     ,'StudyArea209'
-  //                 ]
+//     WA.nav.goToPage('https://play.vs.katariba.online/_/global/play.vs.katariba.online/maps/room-k/map.json');
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+    let ua = window.navigator.userAgent.toLowerCase();
+    let userName = WA.player.name
+    let triggerMessage
+    
+    let mtgRoomNames = ['melonpann'
+                        ,'rollcake'
+                        ,'creampann'
+                        ,'shiopann'
+                        ,'cinnamonroll'
+                        ,'currypann'
+                        ,'chocolatecookie'
+                        ,'shinryokuRight'
+                        ,'shinryokuLeft'
+                        ,'sanbashi'
+                        ,'roomk-shinden'
+                        ,'Kilimanjaro'
+                        ,'MountFuji'
+                        ,'Everest'
+                        ,'Rocky'
+                        ,'StudyArea100'
+                        ,'StudyArea101'
+                        ,'StudyArea102'
+                        ,'StudyArea103'
+                        ,'StudyArea104'
+                        ,'StudyArea105'
+                        ,'StudyArea106'
+                        ,'StudyArea107'
+                        ,'StudyArea108'
+                        ,'StudyArea200'
+                        ,'StudyArea201'
+                        ,'StudyArea202'
+                        ,'StudyArea203'
+                        ,'StudyArea204'
+                        ,'StudyArea205'
+                        ,'StudyArea206'
+                        ,'StudyArea207'
+                        ,'StudyArea208'
+                        ,'StudyArea209'
+                    ]
 
-  // Androidの6~8までは、JITSIに接続する際に、WAを終了してJITSIに直接REDIRECTさせる
-  // if(  ua.indexOf("android 3.") !== -1
-  //   || ua.indexOf("android 4.") !== -1
-  //   || ua.indexOf("android 5.") !== -1
-  //   || ua.indexOf("android 6.") !== -1
-  //   || ua.indexOf("android 7.") !== -1
-  //   || ua.indexOf("android 8.") !== -1) {
+    // Androidの6~8までは、JITSIに接続する際に、WAを終了してJITSIに直接REDIRECTさせる
+    if(  ua.indexOf("android 3.") !== -1
+      || ua.indexOf("android 4.") !== -1
+      || ua.indexOf("android 5.") !== -1
+      || ua.indexOf("android 6.") !== -1
+      || ua.indexOf("android 7.") !== -1
+      || ua.indexOf("android 8.") !== -1) {
 
-  //     for (let s = 0; s < mtgRoomNames.length; s++){
-  //         let mtgRoom = mtgRoomNames[s]
+        for (let s = 0; s < mtgRoomNames.length; s++){
+            let mtgRoom = mtgRoomNames[s]
 
-  //         WA.room.onEnterLayer('red/red-'+mtgRoom).subscribe(() => {
-  //             triggerMessage = WA.ui.displayActionMessage({
-  //                 message: "タブレット利用者はこっちをタッチして部屋に移動してください",
-  //                 callback: () => {
-  //                     WA.nav.goToPage('https://jitsi.katariba.online/globalroomk'+mtgRoom+'#userInfo.displayName="'+ userName + '"');
-  //                 }
-  //             })
-  //         })
-  //         WA.room.onLeaveLayer('red/red-'+mtgRoom).subscribe(() => {
-  //             triggerMessage.remove();
-  //         })
+            WA.room.onEnterLayer('red/red-'+mtgRoom).subscribe(() => {
+                triggerMessage = WA.ui.displayActionMessage({
+                    message: "タブレット利用者はこっちをタッチして部屋に移動してください",
+                    callback: () => {
+                        WA.nav.goToPage('https://jitsi.katariba.online/globalroomk'+mtgRoom+'#userInfo.displayName="'+ userName + '"');
+                    }
+                })
+            })
+            WA.room.onLeaveLayer('red/red-'+mtgRoom).subscribe(() => {
+                triggerMessage.remove();
+            })
 
-  //     }
+        }
 
-  // }
+    }
 
-  
-  for (let s = 36; s < 38; s++){
-      // let roomNum = ("0" + s).slice(-2)
-      // let meetingID = zoomMeetingIDs.roomNum
-      // let url = "https://zoom-sdk-web.vercel.app:/meeting.html?name=Q0ROMi45LjBNYWMjY2hyb21lLzEwNi4wLjAuMA%3D%3D&mn="+meetingID+"&email=&pwd=&role=0&lang=jp-JP&signature=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZGtLZXkiOiJMazkzbUdGRDNaUFB5aUtNc05IQ1psbTd2NFh6MENiTGFNZW8iLCJpYXQiOjE2Njg5NDcxMTUsImV4cCI6MTY2ODk1NDMxNSwibW4iOjM3MTUyNzM2OTQsInJvbGUiOjB9.KwHtUWC1RqAoIxu45fAPp7cUC7w3cqjJuIOiVn49GJs&china=0&sdkKey=Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo"
-
-      // WA.room.setProperty('zone/zoomURL/zoom'+roomNum, "openWebsite", url);
-      // WA.room.onEnterLayer('zone/zoomURL/zoom'+roomNum).subscribe(() => {
-      //   coWebsite = WA.nav.openCoWebSite('https://www.wikipedia.org/');
-      //     // triggerMessage = WA.ui.displayActionMessage({
-      //     //     message: 'SpaceキーかタッチでZoom'+roomNum+'を開きます~~11',
-      //     //     callback: () => {
-      //     //       coWebsite = await WA.nav.openCoWebSite('https://www.wikipedia.org/');
-      //     //     }
-      //     // });
-      // })
-      // WA.room.onLeaveLayer('zone/zoomURL/zoom'+roomNum).subscribe(() => {
-      //   // coWebsite = WA.nav.openCoWebSite('https://www.wikipedia.org/');
-      //   coWebsite.close();
-      //   // triggerMessage.remove();
-      // })
-
-  }
-  
+    
+    for (let s = 4; s < 44; s++){
+        let roomNum = ("0" + s).slice(-2)
+        WA.room.onEnterLayer('zone/zoomURL/zoom'+roomNum).subscribe(() => {
+            triggerMessage = WA.ui.displayActionMessage({
+                message: 'SpaceキーかタッチでZoom'+roomNum+'に移動します',
+                callback: () => {
+                    WA.nav.goToPage('http://zoom'+roomNum+'.katariba.in');
+                }
+            });
+        })
+        WA.room.onLeaveLayer('zone/zoomURL/zoom'+roomNum).subscribe(() => {
+            triggerMessage.remove();
+        })
+    }
+    
+    // for (let s = 44; s < 100; s++){
+    //     let roomNum = ("0" + s).slice(-2)
+    //     let roomID = zoomRoomIDs[roomNum] === undefined ? "0000000000" : zoomRoomIDs[roomNum]
+    //     let signature
+    //     // let signature = signaturesAttendee[roomNum] === undefined ? "0000000000" : signaturesAttendee[roomNum]
+    //     //表示名をEncodeする
+    //     let encodedUserName = btoa(encodeURIComponent(userName).replace(/%([0-9A-F]{2})/g,function toSolidBytes(match,p1) {return String.fromCharCode("0x" + p1);}));
+    //     let accessURL = `https://zoom-sdk-web.vercel.app:/meeting.html?name=${encodedUserName}&mn=${roomID}&email=&pwd=&role=0&lang=jp-JP&signature=${signature}&china=1&sdkKey=Lk93mGFD3ZPPyiKMsNHCZlm7v4Xz0CbLaMeo`
+    //     WA.room.setProperty('zone/zoomURL/zoom'+roomNum, "openWebsite", accessURL);
+    //     WA.room.setProperty('zone/zoomURL/zoom'+roomNum, "openWebsiteTrigger", "onaction");
+    //     WA.room.setProperty('zone/zoomURL/zoom'+roomNum, "openWebsiteTriggerMessage", 'SpaceキーかタッチでZoom'+roomNum+'を開きます');
+    // }
+    
 }).catch(e => console.error(e));
+
